147c4616d0161a73648b683d92ff3a6d76089f90
Paulo Henrique de Meneses Oliveira <ph.1928@hotmail.com>
2023-10-31 14:33:50 -0300

    Add tests to user authentication

--- spec/system/authentication/user_sign_up_as_host_spec.rb
+++ spec/system/authentication/user_sign_up_as_host_spec.rb
@@ -8,7 +8,6 @@ describe 'User sign up as host' do
     visit root_path
     click_on 'Entrar'
     click_on 'Criar conta'
-    select 'Dono de pousada', from: 'Tipo de conta'
 
     # Assert
     expect(page).to have_field 'Tipo de conta'
@@ -16,5 +15,32 @@ describe 'User sign up as host' do
     expect(page).to have_field 'Senha'
     expect(page).to have_field 'Confirme sua senha'
     expect(page).to have_button 'Salvar'
+  end
+
+  it 'successfully' do
+    visit root_path
+    click_on 'Entrar'
+    click_on 'Criar conta'
+    select 'Dono de pousada', from: 'Tipo de conta'
+    fill_in 'E-mail', with: 'exemplo@mail.com'
+    fill_in 'Senha', with: 'password'
+    fill_in 'Confirme sua senha', with: 'password'
+    click_on 'Salvar'
+
+    expect(page).to have_content 'Você realizou seu registro com sucesso!'
+    expect(page).to have_content 'exemplo@mail.com'
+  end
+
+  it 'and submits blank field' do
+    visit root_path
+    click_on 'Entrar'
+    click_on 'Criar conta'
+    select 'Dono de pousada', from: 'Tipo de conta'
+    fill_in 'E-mail', with: 'exemplo@mail.com'
+    fill_in 'Senha', with: 'password'
+    fill_in 'Confirme sua senha', with: ''
+    click_on 'Salvar'
+
+    expect(page).to have_content 'Não foi possível salvar usuário'
   end
 end
---
+++ spec/system/guesthouses/user_register_guesthouse_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+describe 'User registers guest house' do
+  it 'from the home page' do
+    # Arrange
+
+  end
+end
