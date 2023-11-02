792258b6de77e76d58f20dadcbf1766d5f0813e0
Paulo Henrique de Meneses Oliveira <ph.1928@hotmail.com>
2023-11-02 19:32:24 -0300

    Add address unit tests

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,5 +1,77 @@
 require 'rails_helper'
 
 RSpec.describe Address, type: :model do
-  pending 'presence tests'
+  describe '#valid?' do
+    it 'returns false when street_name is empty' do
+      # Arrange
+      address = Address.create(street_name: '', number: '30',
+                                neighbourhood: 'Santa Helena',
+                                city: 'Pulomiranga', state: 'RN',
+                                postal_code: '99000-525')
+
+      # Assert
+      expect(address).not_to be_valid
+    end
+
+    it 'returns false when number is empty' do
+      # Arrange
+      address = Address.create(street_name: 'Rua das Pedras', number: '',
+                                neighbourhood: 'Santa Helena',
+                                city: 'Pulomiranga', state: 'RN',
+                                postal_code: '99000-525')
+
+      # Assert
+      expect(address).not_to be_valid
+    end
+
+    it 'returns false when neighbourhood is empty' do
+      # Arrange
+      address = Address.create(street_name: 'Rua das Pedras', number: '30',
+                                neighbourhood: '',
+                                city: 'Pulomiranga', state: 'RN',
+                                postal_code: '99000-525')
+
+      # Assert
+      expect(address).not_to be_valid
+    end
+
+    it 'returns false when city is empty' do
+      # Arrange
+      address = Address.create(street_name: 'Rua das Pedras', number: '30',
+                                neighbourhood: 'Santa Helena',
+                                city: '', state: 'RN',
+                                postal_code: '99000-525')
+
+      # Assert
+      expect(address).not_to be_valid
+    end
+
+    it 'returns false when state is empty' do
+      # Arrange
+      address = Address.create(street_name: 'Rua das Pedras', number: '30',
+                                neighbourhood: 'Santa Helena',
+                                city: 'Pulomiranga', state: '',
+                                postal_code: '99000-525')
+
+      # Assert
+      expect(address).not_to be_valid
+    end
+
+    it 'returns false when postal_code is empty' do
+      # Arrange
+      address = Address.create(street_name: 'Rua das Pedras', number: '30',
+                                neighbourhood: 'Santa Helena',
+                                city: 'Pulomiranga', state: 'RN',
+                                postal_code: '')
+
+      # Assert
+      expect(address).not_to be_valid
+    end
+  end
 end
+
+
+# address = Address.create!(street_name: 'Rua das Pedras', number: '30',
+#                           neighbourhood: 'Santa Helena',
+#                           city: 'Pulomiranga', state: 'RN',
+#                           postal_code: '99000-525')
