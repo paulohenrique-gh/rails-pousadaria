8157efdb3885d7b4e3b0ff7b4373b0a3c4e5af0a
Paulo Henrique de Meneses Oliveira <ph.1928@hotmail.com>
2023-11-02 17:04:06 -0300

    Refactor guesthouse model

--- app/models/guesthouse.rb
+++ app/models/guesthouse.rb
@@ -8,17 +8,4 @@ class Guesthouse < ApplicationRecord
   enum status: { active: 0, inactive: 1 }
 
   accepts_nested_attributes_for :address
-
-  def pet_policy_description
-    return 'Aceita pets' if self.pet_policy
-    'Não aceita pets'
-  end
-
-  def formatted_checkin_time
-    self.checkin_time.strftime('%H:%M')
-  end
-
-  def formatted_checkout_time
-    self.checkout_time.strftime('%H:%M')
-  end
 end
