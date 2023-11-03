b0f3602642d90bf7b4145748575cc3d692418da5
Paulo Henrique de Meneses Oliveira <ph.1928@hotmail.com>
2023-11-03 09:42:39 -0300

    Add guesthouse model unit test

--- spec/models/guesthouse_spec.rb
+++ spec/models/guesthouse_spec.rb
@@ -121,6 +121,21 @@ RSpec.describe Guesthouse, type: :model do
       expect(guesthouse).not_to be_valid
     end
 
-    pending 'returns true when all required arguments are given'
+    it 'returns true when all required arguments are given' do
+      user = User.create!(email: 'exemplo@mail.com', password: 'password')
+      address = Address.create!(street_name: 'Rua das Pedras', number: '30',
+                                neighbourhood: 'Santa Helena',
+                                city: 'Pulomiranga', state: 'RN',
+                                postal_code: '99000-525')
+
+      guesthouse = Guesthouse.create(brand_name: 'Pousada Bosque',
+                                     corporate_name: 'Pousada Ramos Faria LTDA',
+                                     registration_number: '02303221000152',
+                                     phone_number: '1130205000',
+                                     email: 'atendimento@pousadabosque',
+                                     address: address, user: user)
+
+      expect(guesthouse).to be_valid
+    end
   end
 end
